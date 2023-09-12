e03af053f9bd875c14adc281eee90a97a5085a72
Cherrypi73 <passosbarbosademoraesmaria@gmail.com>
2023-09-12 16:59:02 -0400

    git connection change

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@ const { Sequelize } = require('sequelize');
 const config = require('../config/config')
 require('dotenv').config();
 
-const sequelize = new Sequelize(    );
+const sequelize = new Sequelize(  config.development  );
 
 try {
   sequelize.authenticate();
-  console.log('              ');
+  console.log(' Conectado com o banco de dados ');
 } catch (error) {
-  console.error('             ', error);
+  console.error(' Atenção, a conexão falhou', error);
 }
 
 module.exports = { Sequelize, sequelize };
