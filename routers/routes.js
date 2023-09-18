4257154f345dbcdb79588f8da80cdef82b35c9b5
Maylla Nascimento <maylla.rie@gmail.com>
2023-09-18 11:17:52 -0300

    git routes

--- routers/routes.js
+++ routers/routes.js
@@ -5,12 +5,8 @@ const userController = require('../controllers/userController');
 routes.post('/user', userController.createUser)
 routes.get('/users', userController.findUsers);
 routes.delete('/user/:id', userController.deleteUser);
-/*
-routes.get('/users', userController.findUsers);
-routes.post('/user', userController.createUser);
-routes.delete('/user/:id', userController.deleteUser);
 routes.put('/user/:id', userController.updateUser);
 routes.post('/user/authenticated', userController.authenticatedUser);
-*/
+
 
 module.exports = routes;
