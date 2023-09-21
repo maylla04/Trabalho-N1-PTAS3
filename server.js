f200376c2778cebe4a27d30530f682ad6fe679b3
maylla04 <maylla.rie@gmail.com>
2023-09-21 16:53:43 -0400

    git image

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3003;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.urlencoded({ extended: false }));
 
 
